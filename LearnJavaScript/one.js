696c9dcba2891c27b6db189ec598977d3a7c137f
riceh3 <riceh3@uni.coventry.ac.uk>
2017-02-23 18:02:24 +0000

    javaScript

---
+++ LearnJavaScript/one.js
@@ -0,0 +1,14 @@
+
+//-------------Variables--------------------------
+
+var myName = "Hannah";//visible to whole function
+var undefined = ''; //No value will return undefined
+let myOtherName = "Hannah"; //only visible to loop
+
+
+//-------------Operators-------------------------
+
+var firstHalf = "this is a";
+var secondHalf = "sentence";
+var sentence = (firstHalf + " " + secondHalf);
+console.log(sentence);
