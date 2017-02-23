4233ceab49709a84bc048a4a2307cd51d0e8ab57
riceh3 <riceh3@uni.coventry.ac.uk>
2017-02-23 19:15:49 +0000

    one

--- LearnJavaScript/one.js
+++ LearnJavaScript/one.js
@@ -11,4 +11,33 @@ let myOtherName = "Hannah"; //only visible to loop
 var firstHalf = "this is a";
 var secondHalf = "sentence";
 var sentence = (firstHalf + " " + secondHalf);
-console.log(sentence);
+
+// == IS Equal to
+// === IS
+
+//------------Control Structures-----------------
+
+var pet1 = "cat";
+if (pet1 == "dog"){
+    console.log(pet1 += "!");
+
+}
+else if (pet1 == "cat"){
+    console.log(pet1 += "!!!!!!");
+}
+else {
+    console.log(pet1 += "wrong");
+}
+//-----------Switch case break -- Control Structures----
+var pet = "dog";
+switch (pet) {
+    case "cat": text = "cat";
+    break;
+    
+    case "dog": text = "dog"
+    break;
+
+    default: text = "The pet is not a dog or a cat";
+
+}
+console.log(text);
