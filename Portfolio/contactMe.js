9d6ce63ac12e73bf34e72c30c441f7a789e05c5e
riceh3 <riceh3@uni.coventry.ac.uk>
2017-02-23 12:14:55 +0000

    contactjs

---
+++ Portfolio/contactMe.js
@@ -0,0 +1,23 @@
+
+var firstName = '';
+var lastName = '';
+
+function submitForm(){
+    'use strict';
+
+    var fName = document.getElementById("firstName");
+    var lName = document.getElementById("lastName");
+
+    message = ('Message From: ' + fName + '' + lName);
+    console.log(message);
+    
+    return false;
+
+
+}
+
+function init(){
+    'use strict';
+
+    document.getElementById()
+}
