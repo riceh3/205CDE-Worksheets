a3ab622d35cfaeb45b57312227afd3e600f6d6c3
riceh3 <riceh3@uni.coventry.ac.uk>
2017-03-02 14:41:18 +0000

    homesearch

---
+++ Portfolio/JavaScript/home.js
@@ -0,0 +1,20 @@
+var box = 'Search';
+var output = '';
+
+function startSearch(){
+    'use strict';
+
+    var search = document.getElementById('search')
+    output = (search.value);
+
+    console.log("Searching : " + " " + output);
+    return false;
+
+}
+
+function init(){
+    'use strict';
+
+    document.getElementById('searchbox').onsubmit = startSearch;
+}
+window.onload = init;
