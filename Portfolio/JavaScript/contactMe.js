b0b5e8f27358ea6246de4b781e4182456d2c1c36
riceh3 <riceh3@uni.coventry.ac.uk>
2017-03-02 09:14:09 +0000

    commentform

--- Portfolio/JavaScript/contactMe.js
+++ Portfolio/JavaScript/contactMe.js
@@ -1,19 +1,28 @@
 var firstName = '';
 var lastName = '';
+var emailAddress = '';
+var userMessage = '';
 
 function submitForm(){
     'use strict';
 
     var first = document.getElementById('firstName');
     var last = document.getElementById('lastName');
+    var address = document.getElementById('email');
+    var message = document.getElementById('comment');
 
-
-    var message = '';
 
     firstName = (first.value);
     lastName = (last.value);
+    emailAddress = (address.value);
+    userMessage = (message.value);
 
-    console.log("message from " + " " + firstName + " " + lastName);
+    console.log("message from : " + " " + firstName + " " + lastName);
+    console.log("Email Address : " + " " + emailAddress);
+    console.log("Message : ");
+    console.log("       " + userMessage);
+    console.log("       ");
+
 
     return false;
 
