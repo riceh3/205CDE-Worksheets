3575a46dafc0350f0f201c4e56cbf411d79bed42
riceh3 <riceh3@uni.coventry.ac.uk>
2017-03-09 13:29:46 +0000

    nearlydone

--- Portfolio/JavaScript/cv.js
+++ Portfolio/JavaScript/cv.js
@@ -1,39 +1,5 @@
-window.onload = init;
 
-function xp(){
-    'use strict';
-
-    var newDiv = document.getElementById("work");
-    //newDiv.onmouseover = addText;
-
-}
-function addText(){
-    if (document.createTextNode){
-        var newText = "More info on experience page";
-        var newNode = document.createTextNode(newText);
-        document.getElementById("create").appendChild(newNode);
-    }
-    else {
-        alert("out of date browser");
-    }
-
-}
-
-function init(){
-    'use strict';
-    //var skill = document.getElementById("techdown");
-   // var personalprofile = document.getElementById("personaldown");
-    
-    
-   // skill.onclick = showPlease
-    
-    //personalprofile.onclick = showPersonal
-
-    
-    
-}
-
-function showPersonal(){
+function personalDown(){
     'use strict';
 
     var personal = document.getElementById("personal");
@@ -41,17 +7,98 @@ function showPersonal(){
     personal.innerHTML = "Second year undergraduate studying Computing at Coventry Univeristy. Incredibley interested and inspired by the rapidly changing world of technology that we live in today, and I strive to develop and discover the future technical enhancements that habe yet to have been created. My future ambitions are to gain as much experience and practical skills as possible to work in the computer science industry. Main skills are; organisation, communication, team work, time managment and creativity.";
     
 }
-function jesus(){
+function personalUp(){
     'use strict';
 
-    console.log('hova bitch');
+    var personal = document.getElementById("personal");
+    
+    personal.innerHTML = " ";
 }
 
-
-function satan(){
+function techDown(){
     'use strict';
 
-    console.log('pancake');
+    var tech = document.getElementById("list");
+    tech.innerHTML = "<ul><li>Python</li><li>Microsoft Packages</li><li>SQL</li></ul>";
+
+    var tech2 = document.getElementById("list2");
+    tech2.innerHTML = "<ul><li>C++</li><li>GitHub</li><li>Visual Studio</li></ul>";
+
+    var tech3 = document.getElementById("list3");
+    tech3.innerHTML = "<ul><li>C#</li><li>HTML/CSS</li><li>Full UK Driving License</li></ul>";
+
 }
 
+function techUp(){
+    'use strict';
 
+    var tech = document.getElementById("list");
+    tech.innerHTML = " ";
+    
+    var tech2 = document.getElementById("list2");
+    tech2.innerHTML = " ";
+
+    var tech3 = document.getElementById("list3");
+    tech3.innerHTML = " ";
+}
+function educationDown(){
+    'use strict';
+
+    var education = document.getElementById("place");
+    education.innerHTML = "Coventry University(2015-Present)";
+
+    var education2 = document.getElementById("secondyear");
+    education2.innerHTML = "Second Year Modules:";
+
+    var education3 = document.getElementById("modules1");
+    education3.innerHTML = " Designing for Usability 2 - Developing for the modern Web 1 - Programming, Algorithms and Data Structures - Data and Information Retrieval - Technology and it's Social, Legal and Ethical context - Making Money Online";
+
+    var education4 = document.getElementById("firstyear");
+    education4.innerHTML = "First Year Modules:";
+
+    var education5 = document.getElementById("modules2");
+    education5.innerHTML = " Introduction to Computing(72%) - Introduction to Algorithms(68%) - Computer Architecture and Networks(60%) - Enterprise Information Systems(78%) - Designing for Usability 1(66%) - Logic and Sets(68%) - Foundation App Development with Microsoft Net Framework(100%).";
+
+    var education6 = document.getElementById("place2");
+    education6.innerHTML = " King Edward VI College(2013-2015):";
+
+    var education7 = document.getElementById("kegs");
+    education7.innerHTML = "4 AS-Level and 3 A-Level(Accounting, Law, Business)";
+
+    var education8 = document.getElementById("place3");
+    education8.innerHTML = "Etone College(2011-2013)";
+
+    var education9 = document.getElementById("etone");
+    education9.innerHTML = "10 GCSE'S graded between A-C, including English and Math's";
+
+}
+function educationUp(){
+    'use strict';
+
+    var education = document.getElementById("place");
+    education.innerHTML = " ";
+
+    var education2 = document.getElementById("secondyear");
+    education2.innerHTML= " ";
+
+    var education3 = document.getElementById("modules1");
+    education3.innerHTML = " ";
+
+    var education4 = document.getElementById("firstyear");
+    education4.innerHTML = " ";
+
+    var education5 = document.getElementById("modules2");
+    education5.innerHTML = " ";
+
+    var education6 = document.getElementById("place2");
+    education6.innerHTML = " ";
+
+    var education7 = document.getElementById("kegs");
+    education7.innerHTML = " ";
+
+    var education8 = document.getElementById("place3");
+    education8.innerHTML = " ";
+
+    var education9 = document.getElementById("etone");
+    education9.innerHTML = " ";
+}
