f79340b5c62bd2fbd0c9c5909e73a641ec72afdf
riceh3 <riceh3@uni.coventry.ac.uk>
2017-03-09 10:21:15 +0000

    javadisbitch

--- Portfolio/JavaScript/cv.js
+++ Portfolio/JavaScript/cv.js
@@ -1,3 +1,40 @@
-function init(){
-    var 
+window.onload = init();
+
+function xp(){
+    'use strict';
+
+    var newDiv = document.getElementById("work");
+    //newDiv.onmouseover = addText;
+
 }
+function addText(){
+    if (document.createTextNode){
+        var newText = "More info on experience page";
+        var newNode = document.createTextNode(newText);
+        document.getElementById("create").appendChild(newNode);
+    }
+    else {
+        alert("out of date browser");
+    }
+
+}
+function init(){
+    'use strict';
+
+    var personalprofile = document.getElementById("personalarrow");
+    personalprofile.onclick = showPersonal;
+}
+function showPersonal(){
+    'use strict';
+
+    var show = document.getElementById("show");
+    
+    show.innerHTML = "Second year undergraduate studying Computing at Coventry Univeristy. Incredibley interested and inspired by the rapidly changing world of technology that we live in today, and I strive to develop and discover the future technical enhancements that habe yet to have been created. My future ambitions are to gain as much experience and practical skills as possible to work in the computer science industry. Main skills are; organisation, communication, team work, time managment and creativity.";
+
+    }
+
+
+
+
+
+
